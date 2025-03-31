293f27720a5e5bbba38da10a14b94fee0cef741f
Pedro Fuentes <pedrofuentes7799@gmail.com>
2025-03-31 22:35:32 +0000

    Template hai

--- taller1/ejercicio/src/hai.c
+++ taller1/ejercicio/src/hai.c
@@ -7,7 +7,40 @@
 #include <stdlib.h>
 #include <errno.h>
 
+
+int pid_padre;
+int pid_hijo;
+
+
+
+int sigint_handler(int signal){
+	// espero a que mi hijo termine
+	wait(NULL);
+}
+
+
 int main(int argc, char* argv[]) {
-  	// Completar
-	return 0;
+    if (argc != 2) {
+        printf("Uso: %s <comando>\n", argv[0]);
+        return 1;
+    }
+
+    char* comando = argv[1];
+
+	signal(SIGINT, sigint_handler)
+
+
+
+	// ejecutar hijo, con un clone, guardarme pid
+
+	for (int i=0; i<5;i++){
+		printf("sup!\n");
+		kill(pid_hijo, SIGURG);
+
+
+	}
+
+
+
+
 }
