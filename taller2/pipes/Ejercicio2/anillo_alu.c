c33279f30f2f9845e1a3a6e213bc33db78811f41
Pedro Fuentes <pedrofuentes7799@gmail.com>
2025-04-07 22:25:53 +0000

    primera implementacion anillo

--- taller2/pipes/Ejercicio2/anillo_alu.c
+++ taller2/pipes/Ejercicio2/anillo_alu.c
@@ -5,13 +5,11 @@
 #include <sys/wait.h>
 #include <time.h>
 #include "constants.h"
+enum {READ, WRITE};
+int status, pid, n, start, c, secret_number;
 
 int generate_random_number(){
 	return (rand() % 50);
-}
-
-void correHijo(){
-	while(1){}
 }
 
 int main(int argc, char **argv)
@@ -19,14 +17,15 @@ int main(int argc, char **argv)
 	//Funcion para cargar nueva semilla para el numero aleatorio
 	srand(time(NULL));
 
-
-	int status, pid, n, start, buffer;
 	n = atoi(argv[1]);
-	buffer = atoi(argv[2]);
+	c = atoi(argv[2]);
 	start = atoi(argv[3]);
 
-	// n*2 pipes para hacer el anillo + conexion entre los hijos con el padre 
-	int pipes[n*2][2];
+	// n pipes (por ser anillo) y uno mas para la comunicacion padre->start
+	int pipes[n+1][2];
+	for (int i = 0; i<n+1; i++){
+		pipe(pipes[i]);
+	}
 	
 	// lista de hijos
 	pid_t *hijos = malloc(sizeof(*hijos) * n);
@@ -34,16 +33,59 @@ int main(int argc, char **argv)
 	if (argc != 4){ printf("Uso: anillo <n> <c> <s> \n"); exit(0);}
     
   	/* COMPLETAR */
-    printf("Se crearán %i procesos, se enviará el caracter %i desde proceso %i \n", n, buffer, start);
+    printf("Se crearán %i procesos, se enviará el caracter %i desde proceso %i \n", n, c, start);
     
 	for(int i = 0; i < n; i++){
 		pid_t pid = fork();
-		hijos[i] = pid;
 		if(pid == 0){
-			correHijo();
+			// cierro los pipes que no uso
+			for (int j = 0; j<n+1; j++){
+				if (i==j){
+					close(pipes[j][READ]);
+				} else if (j == i-1){
+					close(pipes[j][WRITE]);
+				}
+				else if (i != start){
+					close(pipes[j][WRITE]);
+					close(pipes[j][READ]);
+				}
+			}
+			int current_number;
+			if (i == start){
+				read(pipes[n][READ], &current_number, sizeof(int));
+				secret_number = generate_random_number();
+				printf("El numero secreto es... %d", secret_number);
+
+				// inicio la comunicacion
+				current_number++;
+				write(pipes[i][WRITE], &current_number, sizeof(secret_number));
+			}
+			
+			while(read(pipes[i-1][READ], current_number, sizeof(int)) == sizeof(int)){	
+				if(i == start && current_number > secret_number){
+					// le mando al padre por el pipe n que ya termine
+					write(pipes[n][WRITE], current_number, sizeof(current_number));
+
+					// cierro el pipe
+					close(pipes[i][WRITE]);
+					}
+				else {
+					current_number++;
+					write(pipes[i][WRITE], current_number, sizeof(int));
+				}
+			}
+			close(pipes[i-1][READ]);
+			close(pipes[i][WRITE]);
+			exit(EXIT_SUCCESS);
+				
+		} else {
+			hijos[i] = pid;
 		}
-		
 	}
-    /* COMPLETAR */
+
+	write(pipes[n][WRITE], &c, sizeof(c));
+	int result;
+	read(pipes[n][READ], &result, sizeof(result));
+	printf("El resultado es %d\n", result);
     
 }
