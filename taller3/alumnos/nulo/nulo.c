ef756c85034d4b16960f292373e194a2f60b0679
Pedro Fuentes <pedrofuentes7799@gmail.com>
2025-05-18 21:05:59 +0000

    Implementa /dev/nulo. Pasan los tests :D

--- taller3/alumnos/nulo/nulo.c
+++ taller3/alumnos/nulo/nulo.c
@@ -10,12 +10,13 @@ static ssize_t nulo_read(struct file *filp, char __user *data, size_t s, loff_t 
 }
 
 static ssize_t nulo_write(struct file *filp, const char __user *data, size_t s, loff_t *off) {
-  return 0;
+  return s;  
 }
 
 static struct file_operations nulo_operaciones = {
   .owner = THIS_MODULE,
-  // Completar
+  .read = nulo_read,
+  .write = nulo_write,
 };
 
 static struct cdev nulo_device;
@@ -23,14 +24,23 @@ static dev_t major;
 static struct class *nulo_class;
 
 static int __init hello_init(void) {
+  // registrar dispositivo
+  major = register_chrdev(0, "nulo", &nulo_operaciones);
+  nulo_class = class_create(THIS_MODULE, "nulo");
+
+  cdev_init(&nulo_device, &nulo_operaciones);
+  cdev_add(&nulo_device, major, 1);
+
+  device_create(nulo_class, NULL, MKDEV(major, 0), NULL, "nulo");
   
-  /* Completar */
-	
   return 0;
 }
 
 static void __exit hello_exit(void) {
-  /* Completar */
+  device_destroy(nulo_class, MKDEV(major, 0));
+  cdev_del(&nulo_device);
+  unregister_chrdev(major, "nulo");
+  class_destroy(nulo_class);
 }
 
 module_init(hello_init);
