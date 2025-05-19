b37f82aa69d3f9562122c93e877ef8e40782d28b
Pedro Fuentes <pedrofuentes7799@gmail.com>
2025-05-19 17:35:55 +0000

    Version con spinlock

--- taller3/alumnos/letras123/letras123.c
+++ taller3/alumnos/letras123/letras123.c
@@ -10,43 +10,87 @@
 
 #define SLOT_COUNT 3
 
-static spinlock_t lock;
+static int current_users = 0;
+static spinlock_t current_users_lock;
 
-typedef struct user_data {
+
+// Decidi usar un spinlock en vez de un semaforo ya que las operaciones que se van a hacer 
+// cuando esta puesto el lock son muy simples (incrementar y decrementar un contador)
+
+typedef struct user_data {  
     bool valid;
     char input;
+    spinlock_t lock;
 } user_data;
 
 static int letras_open(struct inode *inod, struct file *filp) {
+    user_data *udata;
 
-    /* Completar */
+    spin_lock(&current_users_lock);
+    if (current_users >= SLOT_COUNT){
+        spin_unlock(&current_users_lock);
+        return -EPERM;
+    } else {
+        current_users++;
+        spin_unlock(&current_users_lock);
+    }
 
-    filp->private_data = kmalloc(sizeof(struct user_data), GFP_KERNEL);
-    ((user_data *) filp->private_data)->valid = false;
+    // inicializo la estructura, invalida hasta que se escriba
+    filp->private_data = kmalloc(sizeof(user_data), GFP_KERNEL);
+    udata = (user_data *) filp->private_data;
+    udata->valid = false; 
+    spin_lock_init(&(udata->lock));
+
     return 0;
- 
 }
 
 static int letras_release(struct inode *inod, struct file *filp) {
-
-    /* Completar */
-
+    spin_lock(&current_users_lock);
+    current_users--;
+    spin_unlock(&current_users_lock);
+    
+    kfree(filp->private_data);
     return 0;
 }
 
 static ssize_t letras_read(struct file *filp, char __user *data, size_t size, loff_t *offset) {
     user_data *udata = (user_data *) filp->private_data;
+    char *char_copies;
+    int i;
+ 
+    // bloqueo para que no se pueda leer hasta que no se termine de escribir. 
+    spin_lock(&(udata->lock));
+    if (!udata->valid) {
+        spin_unlock(&(udata->lock));
+        return -EPERM;
+    }
+    char_copies = kmalloc(size, GFP_KERNEL);
+    for (i = 0; i < size; i++) {
+        char_copies[i] = udata->input;
+    }
 
-    /* Completar */
-   
-    return -EPERM;
+    spin_unlock(&(udata->lock));
+
+    copy_to_user(data, char_copies, size);
+    kfree(char_copies);
+    return size;
 }
 
 static ssize_t letras_write(struct file *filp, const char __user *data, size_t size, loff_t *offset) {
     user_data *udata = (user_data *) filp->private_data;
 
-    /* Completar */
+    // bloqueo para que no se pueda escribir hasta que no se termine de leer. 
+    spin_lock(&(udata->lock));
+    // solo se puede escribir el primer caracter. Si ya se habia escrito, se ignora.
+    if (!(udata->valid)) {
 
+        // guardar el primer caracter escrito
+        copy_from_user(&(udata->input), data, 1);
+        udata->valid = true;
+        
+    }
+    
+    spin_unlock(&(udata->lock));
     return size;
 }
 
@@ -63,7 +107,7 @@ static struct cdev letras_cdev;
 static struct class *letras_class;
 
 static int __init letras_init(void) {
-    spin_lock_init(&lock);
+    spin_lock_init(&current_users_lock);
 
     cdev_init(&letras_cdev, &letras_fops);
     alloc_chrdev_region(&letras_devno, 0, 1, "SO-letras123");
